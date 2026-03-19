10ba1578e3a8f599fd5a392b7bd35047dfb60b2f
tissad <tissad@student.42.fr>
2026-03-20 00:55:47 +0100

    level06

--- commands/comands.cmd
+++ commands/comands.cmd
@@ -1,2 +1,4 @@
 ssh -p 4243 level00@localhost 'bash -s' < ressources/find_ssh_port.sh
 ssh -p 4243 level00@localhost 'bash -s' < ressources/find_flag00.sh
+
+scp -P 4243 level02@192.168.1.183:level02.pcap .
